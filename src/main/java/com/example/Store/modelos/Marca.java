2a874e2cb26fcca1c56bcb9e1d91eea6ed3a2751
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-03-20 12:08:27 -0500

    tienda

--- src/main/java/com/example/Store/modelos/Marca.java
+++ src/main/java/com/example/Store/modelos/Marca.java
@@ -12,9 +12,23 @@ public class Marca {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name="nombreMaraca",nullable = false,length = 50)
     private String nombreMarca;
+    @Column(name="nit",nullable = false,length = 20)
     private String nit;
+    @Column(name="anoCreacion",nullable = false)
     private LocalDate anoCreacion;
+    @Column(name="sedePrincipal",nullable = false,length = 50)
     private String sedePrincipal;
 
+    public Marca() {
+    }
+
+    public Marca(Integer id, String nombreMarca, String nit, LocalDate anoCreacion, String sedePrincipal) {
+        this.id = id;
+        this.nombreMarca = nombreMarca;
+        this.nit = nit;
+        this.anoCreacion = anoCreacion;
+        this.sedePrincipal = sedePrincipal;
+    }
 }
--- src/main/java/com/example/Store/modelos/Producto.java
+++ src/main/java/com/example/Store/modelos/Producto.java
@@ -8,11 +8,33 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name="nombresProducto",nullable = false,length = 40)
     private String nombreProducto;
+    @Column(name="referencia",nullable = false,length = 20)
     private String referencia;
+    @Column(name="talla",nullable = false,length =5)
     private String talla;
+    @Column(name="cantidadBodega",nullable = false)
     private Integer cantidadBodega;
+    @Column(name="precioUnitario",nullable = false)
     private Integer precioUnitario;
+    @Column(name="descripcion",nullable = false,length = 50)
     private String descripcion;
+    @Column(name="fotografia",nullable = false,length = 250)
     private String fotografia;
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, String nombreProducto, String referencia, String talla,
+                    Integer cantidadBodega, Integer precioUnitario, String descripcion, String fotografia) {
+        this.id = id;
+        this.nombreProducto = nombreProducto;
+        this.referencia = referencia;
+        this.talla = talla;
+        this.cantidadBodega = cantidadBodega;
+        this.precioUnitario = precioUnitario;
+        this.descripcion = descripcion;
+        this.fotografia = fotografia;
+    }
 }
